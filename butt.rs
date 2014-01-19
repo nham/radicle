b7d17c6858d15e539b55eb71440bf822009905fe
nham <hamann.nick@gmail.com>
2014-01-18 22:04:14 -0500

    implement Default for ~[~BValue] in order to support BVector, which I just enabled

--- butt.rs
+++ butt.rs
@@ -3,7 +3,15 @@ use std::fmt;
 fn main() {
     let x = eval( Leaf(~"5") );
     println!("{}", x);
-    //let y =
+
+    let y: ~[int] = ~[1, 5, 19, -21];
+
+    for &x in y.iter() {
+        println!("hahahah {}", x);
+    }
+
+    println!("uhhhhhh {}", BVector(~[~BNil, ~BChar('z'), ~BBoolean(true)]));
+
 }
 
 enum Expression {
@@ -19,7 +27,7 @@ enum BValue {
     BChar(char),
     BNumber(f64),
     BString(~str),
-    //BVector
+    BVector(~[~BValue]),
     //BProcedure
     BCons(~BValue, ~BValue),
     BNil
@@ -28,20 +36,36 @@ enum BValue {
 impl fmt::Default for BValue {
     fn fmt(v: &BValue, f: &mut fmt::Formatter) {
         match *v {
-            BBoolean(b) => write!(f.buf, "BBoolean({})", b),
+            BBoolean(b)    => write!(f.buf, "BBoolean({})", b),
             BSymbol(ref s) => write!(f.buf, "BSymbol({})", *s),
-            BChar(c) => write!(f.buf, "BChar({})", c),
-            BNumber(n) => write!(f.buf, "BNumber({})", n),
+            BChar(c)       => write!(f.buf, "BChar({})", c),
+            BNumber(n)     => write!(f.buf, "BNumber({})", n),
             BString(ref s) => write!(f.buf, "BNumber({})", *s),
+            BVector(ref v) => write!(f.buf, "BVector({})", *v),
             BCons(ref v1, ref v2) => write!(f.buf, "BCons({}, {})", **v1, **v2),
             BNil => write!(f.buf, "()")
         }
     }
 }
 
+impl fmt::Default for ~[~BValue] {
+    fn fmt(v: &~[~BValue], f: &mut fmt::Formatter) {
+        write!(f.buf, "[");
+
+        for &ref x in v.iter() {
+            write!(f.buf, " {}", **x);
+
+        }
+
+        write!(f.buf, " ]");
+    }
+}
+
 fn eval(expr: Expression) -> Result<BValue, ~str> {
     match expr {
-        Leaf(x) => Ok(BSymbol(x)),
+        Leaf(x) => {
+            Ok(BSymbol(x))
+        },
         _       => Err(~"not implemented")
     }
 }
