6c583b7633e4e4a1d0c8f51452be3ec04f02fcae
nham <hamann.nick@gmail.com>
2014-01-25 13:45:53 -0500

    move tree code to a separate file

--- radicle.rs
+++ radicle.rs
@@ -6,6 +6,8 @@ use tree::Tree;
 use Atom = tree::Leaf;
 use List = tree::Branch;
 
+mod tree;
+
 fn main() {
 /*
     read_eval("5678)");
@@ -279,66 +281,4 @@ fn eval(expr: Expression) -> Result<Expression, ~str> {
                 */
         }
     }
-}
-
-
-mod tree {
-    use std::fmt::{Default, Formatter};
-
-    #[deriving(Eq, Clone)]
-    pub enum Tree<T> {
-        Leaf(T),
-        Branch(~[Tree<T>])
-    }
-
-    impl<T> Tree<T> {
-        pub fn is_leaf(&self) -> bool {
-            match *self {
-                Leaf(_) => true,
-                _       => false
-            }
-        }
-
-        pub fn is_branch(&self) -> bool {
-            !self.is_leaf()
-        }
-
-        pub fn get_ref_leaf<'a>(&'a self) -> &'a T {
-            match *self {
-                Leaf(ref val) => val,
-                _         => fail!("called Tree<T>::unwrap_leaf() on Branch()"),
-            }
-        }
-
-        pub fn unwrap_branch(self) -> ~[Tree<T>] {
-            match self {
-                Branch(val) => val,
-                _         => fail!("called Tree<T>::unwrap_branch() on Branch()"),
-            }
-        }
-
-    }
-
-    impl<T: Default> Default for Tree<T> {
-        fn fmt(v: &Tree<T>, f: &mut Formatter) {
-            match *v {
-                Branch(ref vec) => write!(f.buf, "Branch{}", *vec),
-                Leaf(ref val) => write!(f.buf, "Leaf({})", *val)
-            }
-        }
-    }
-
-    impl<T: Default> Default for ~[Tree<T>] {
-        fn fmt(v: &~[Tree<T>], f: &mut Formatter) {
-            write!(f.buf, "[");
-
-            for x in v.iter() {
-                write!(f.buf, " {}", *x);
-
-            }
-
-            write!(f.buf, " ]");
-        }
-    }
-
 }
