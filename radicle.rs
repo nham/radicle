fa77b19a8b2995f8bf4ea852299ed86fc862f60a
nham <hamann.nick@gmail.com>
2014-01-27 17:01:48 -0500

    add test case for eval()ing quote expressions

--- radicle.rs
+++ radicle.rs
@@ -562,16 +562,43 @@ fn test_eval_symbol() {
     let bar = Atom(~"bar");
     let bar2 = bar.clone();
     env.bindings.insert(~"foo", bar);
-    assert!( eval(Atom(~"foo"), &env).is_ok() );
-    assert!( eval(Atom(~"foo"), &env).unwrap().eq(&bar2) );
+    let foo_eval = eval(Atom(~"foo"), &env);
+    assert!( foo_eval.is_ok() && foo_eval.unwrap().eq(&bar2) );
 
     let env2 = Environment{parent: Some(&env), bindings: HashMap::new()};
-    assert!( eval(Atom(~"foo"), &env2).is_ok() );
-    assert!( eval(Atom(~"foo"), &env2).unwrap().eq(&bar2) );
+    let foo2_eval = eval(Atom(~"foo"), &env2);
+    assert!( foo2_eval.is_ok() && foo2_eval.unwrap().eq(&bar2) );
 }
 
 #[test]
 fn test_eval_empty_list() {
     let env = Environment{parent: None, bindings: HashMap::new()};
     assert!( eval(List(~[]), &env).is_err() );
+}
+
+#[test]
+fn test_eval_quote() {
+    let mut env = Environment{parent: None, bindings: HashMap::new()};
+
+    let nil = List(~[]);
+    let qnil = List(~[Atom(~"quote"), nil.clone()]);
+    let qnil_eval = eval(qnil, &env);
+    assert!( qnil_eval.is_ok() && qnil_eval.unwrap().eq(&nil) );
+
+    let foo = Atom(~"foo");
+    let qfoo = List(~[Atom(~"quote"), foo.clone()]);
+    let qfoo2 = qfoo.clone();
+    let qfoo_eval = eval(qfoo, &env);
+    assert!( qfoo_eval.is_ok() && qfoo_eval.unwrap().eq(&foo) );
+
+    // "(quote foo)" should evaluate to "foo" regardless of what the symbol foo is
+    // bound to in the environment
+    env.bindings.insert(~"foo", Atom(~"bar"));
+    let qfoo2_eval = eval(qfoo2, &env);
+    assert!( qfoo2_eval.is_ok() && qfoo2_eval.unwrap().eq(&foo) );
+
+    let list = List(~[Atom(~"foo"), Atom(~"bar"), Atom(~"baz")]);
+    let qlist = List(~[Atom(~"quote"), list.clone()]);
+    let qlist_eval = eval(qlist, &env);
+    assert!( qlist_eval.is_ok() && qlist_eval.unwrap().eq(&list) );
 }
